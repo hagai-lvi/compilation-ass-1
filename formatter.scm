0e240d2b5ec031e80be7217b7bb5b0ad05ad129e
hagai-lvi <hagai.lvi@gmail.com>
2014-11-04 19:58:19 +0200

    Added examples and ^wrap

---
+++ formatter.scm
@@ -0,0 +1,66 @@
+(load "pc.scm")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;            examples             ;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define <digit-0-9>
+  (range #\0 #\9))
+
+(define <digit-1-9>
+  (range #\1 #\9))
+
+(define <nat>
+  (new (*parser (char #\0))
+       (*pack (lambda (_) 0))
+
+       (*parser <digit-1-9>)
+       (*parser <digit-0-9>) *star
+       (*caten 2)
+       (*pack-with
+	(lambda (a s)
+	  (string->number
+	   (list->string
+	    `(,a ,@s)))))
+
+       (*disj 2)
+       done))
+
+(define <my-parser>
+	(new (*parser (char #\0))
+		 (*pack (lambda (_) 0))
+	done))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;         end of examples         ;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ (define ^<wrap>
+ 	(lambda (<left> <right>)
+ 		(lambda (<p>)
+ 			(new 	(*parser <left>)
+ 					(*parser <p>)
+ 					(*parser <right>)
+ 					(*caten 3)
+ 					(*pack-with
+ 					(lambda ( < e >) e))
+ 					done
+ 					)
+
+ 		)
+
+ ))
+
+
+(define <left-bracket>
+	(new (*parser (char #\{  ))
+		 (*pack (lambda (_) "{" ))
+	done))
+
+
+(define <right-bracket>
+	(new (*parser (char #\}  ))
+		 (*pack (lambda (_) "}" ))
+	done))
+
+(define x (^<wrap> <left-bracket> <right-bracket>))
+(define y (x <nat>))
