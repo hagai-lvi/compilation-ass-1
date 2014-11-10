4c16bdb2361a5158fe1ac04381b77734a610ada4
Hagai <hagai.lvi@gmail.com>
2014-11-10 20:46:21 +0200

    Update install-petite.sh

--- install-petite.sh
+++ install-petite.sh
@@ -5,7 +5,7 @@ wget http://www.scheme.com/download/pcsv8.4-a6le.tar.gz
 tar xzf pcsv8.4-a6le.tar.gz
 
 cd csv8.4/custom
-
-./configure
+dir=`pwd`
+./configure --installprefix="$dir"
 make
 make install
